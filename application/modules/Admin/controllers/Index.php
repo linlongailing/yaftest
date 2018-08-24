e68160085287f7bdfa9b40f25831c9d5efc4ec75
linlongailing <2622386259@qq.com>
2018-08-24 17:39:38 +0800

    login,register

--- application/modules/Admin/controllers/Index.php
+++ application/modules/Admin/controllers/Index.php
@@ -1,7 +1,12 @@
 <?php
-// 后台首页控制器
-class IndexController  extends Yaf_Controller_Abstract{
-    public function indexAction(){
 
+// 后台首页控制器
+class IndexController extends Yaf_Controller_Abstract
+{
+    public function indexAction()
+    {
+        return true;
     }
+
+
 }
